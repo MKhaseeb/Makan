349f17075666e78c4af2e8e8437ed01307532ba7
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-06-16 11:04:55 +0300

    Project

---
+++ Makan/src/main/java/com/makan/project/MakanApplication.java
@@ -0,0 +1,13 @@
+package com.makan.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakanApplication.class, args);
+	}
+
+}
---
+++ Makan/src/test/java/com/makan/project/MakanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makan.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
