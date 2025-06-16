d5bbf88ea6dc8ce2075e58595aed275fccab21a1
Ibrahemiyad <165409542+Ibrahemiyad@users.noreply.github.com>
2025-06-16 15:04:58 +0300

    models

---
+++ Makan/src/main/java/com/makan/project/models/Rating.java
@@ -0,0 +1,75 @@
+package com.makan.project.models;
+
+public class Rating {
+    private int id;
+    private int venueId;
+    private String userName;
+    private int score;
+    private String comment;
+
+    // Constructors
+    public Rating() {}
+
+    public Rating(int id, int venueId, String userName, int score, String comment) {
+        this.id = id;
+        this.venueId = venueId;
+        this.userName = userName;
+        this.score = score;
+        this.comment = comment;
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVenueId() {
+        return venueId;
+    }
+
+    public void setVenueId(int venueId) {
+        this.venueId = venueId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "id=" + id +
+                ", venueId=" + venueId +
+                ", userName='" + userName + '\'' +
+                ", score=" + score +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+
+}
---
+++ Makan/src/main/java/com/makan/project/models/Service.java
@@ -0,0 +1,112 @@
+package com.makan.project.models;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="services")
+public class Service {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	@Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+	private String name;
+    @NotNull
+	@Min(value = 1, message = "Price must be at least 1")
+	private double price;
+    @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date updatedAt;
+
+    @ManyToMany(mappedBy = "services")
+    private List<Venue> venues;
+
+	public Service() {
+    }
+    
+    public Service(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+
+
+	
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Venue> getVenues() {
+        return venues;
+    }
+
+    public void setVenues(List<Venue> venues) {
+        this.venues = venues;
+    }
+
+}
